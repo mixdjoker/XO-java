f1c3f1fca32025ca8ed58cb1cb13d33d25cc0e28
-=dJoker=- <mix.djoker@gmail.com>
2018-01-21 13:25:15 +1000

    Add Field Class Test

---
+++ test/org/mix/xo/model/FieldTest.java
@@ -0,0 +1,32 @@
+package org.mix.xo.model;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    private final Point inputPoint = new Point(2, 2);
+    private final Figure inputFigureX = Figure.X;
+
+    @Test
+    public void getSize() {
+        final int testValue = 3;
+        final Field field = new Field();
+
+        assertEquals(testValue, field.getSize());
+    }
+
+    @Test
+    public void setFigure() {
+        final Figure expectedFigureX = inputFigureX;
+        final Field field = new Field();
+
+        field.setFigure(inputPoint, inputFigureX);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(expectedFigureX, actualFigure);
+    }
+}
