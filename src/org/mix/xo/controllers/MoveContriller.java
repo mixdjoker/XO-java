33cd21d198a9d25f1105eef7031c9a6569053d82
-=dJoker=- <mix.djoker@gmail.com>
2018-01-21 21:02:19 +1000

    Added MoveController

--- src/org/mix/xo/controllers/MoveContriller.java
+++ src/org/mix/xo/controllers/MoveContriller.java
@@ -1,4 +1,22 @@
 package org.mix.xo.controllers;
 
+import org.mix.xo.model.Field;
+import org.mix.xo.model.Figure;
+import org.mix.xo.model.exception.*;
+
+import java.awt.*;
+
 public class MoveContriller {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure)
+                            throws AlreadyOccupiedException, InvalidPointException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
+
 }
